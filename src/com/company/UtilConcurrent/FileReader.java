87ca4ce26ba9d676433f7e2a5bb5918d92d4a69f
Aleksandr Kretov <a.kretow@ya.ru>
2017-12-14 22:32:34 +0300

    its works

--- src/com/company/UtilConcurrent/FileReader.java
+++ src/com/company/UtilConcurrent/FileReader.java
@@ -1,37 +1,54 @@
 package com.company.UtilConcurrent;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FileReader implements Runnable {
+public class FileReader implements Callable<Result> {
 
     private Lock lock;
     private Path path;
     private String searchString;
+    private AtomicInteger counter;
+    private BufferedWriter bos;
+    private final int PERIOD = 5;
 
-    public FileReader(Lock lock, Path path, String searchString) {
+    public FileReader(Lock lock, Path path, String searchString, AtomicInteger counter, BufferedWriter bos) {
         this.lock = lock;
         this.path = path;
         this.searchString = searchString;
+        this.counter = counter;
+        this.bos = bos;
     }
 
     @Override
-    public void run() {
+    public Result call() {
+        int countLocal = 0;
         try {
             String allFile = new String(Files.readAllBytes(path));
             Pattern pattern = Pattern.compile(searchString);
             Matcher matcher = pattern.matcher(allFile);
-            while (true) {
-
+            while (matcher.find()) {
+                try {
+                    lock.lock();
+                    countLocal++;
+                    if (counter.incrementAndGet() % PERIOD == 0) {
+                        bos.write("Найдено еще " + PERIOD + " страданий. Всего страданий: " + counter.get() + "\n");
+                    }
+                } finally {
+                    lock.unlock();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return new Result(path.getFileName().toString(), countLocal);
     }
 }
--- src/com/company/UtilConcurrent/Start.java
+++ src/com/company/UtilConcurrent/Start.java
@@ -1,7 +1,15 @@
 package com.company.UtilConcurrent;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -9,12 +17,49 @@ public class Start {
 
     public static void main(String[] args) {
 
+        AtomicInteger counter = new AtomicInteger(0);
         FileFounder fileFounder = new FileFounder("txt", "txt");
         List<Path> files = fileFounder.getFileList();
         Lock lock = new ReentrantLock();
 
-        for (Path path : files) {
-            System.out.println(path.getFileName().toString());
+        try (BufferedWriter bos = new BufferedWriter(new FileWriter("counts.txt"));
+                BufferedWriter countsWritre = new BufferedWriter(new FileWriter("howMany.txt"))) {
+            ExecutorService execService = Executors.newFixedThreadPool(files.size());
+            List<Future<Result>> futures = new ArrayList<>();
+            for (int i = 0; i < files.size(); i++) {
+                Future<Result> future = execService.submit(new FileReader(lock, files.get(i), "(С|с)традани(е|я|й|и)", counter, bos));
+                futures.add(future);
+            }
+            while (true) {
+                List<Future<Result>> tempFutures = new ArrayList<>();
+                tempFutures.addAll(0, futures);
+
+                for (Future<Result> future : tempFutures) {
+                    if (future.isDone()) {
+                        try {
+                            //System.out.println(future.get().getFileName() + " >>> " + future.get().getCount());
+                            countsWritre.write(future.get().getFileName() + " >>> " + future.get().getCount() + "\n");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+
+                        futures.remove(future);
+                    }
+                }
+                if (futures.size() == 0) {
+                    execService.shutdown();
+                    countsWritre.flush();
+                    String total = "Всего страданий: " + counter.get();
+                    bos.write(total);
+                    bos.flush();
+                    System.out.println(total);
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
 
